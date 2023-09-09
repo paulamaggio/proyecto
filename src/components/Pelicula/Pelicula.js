6f9a7dc15ea48199081ffc50dc7fd44e5b59be4e
paulamaggio <pmaggio@udesa.edu.ar>
2023-09-09 17:21:35 -0300

    unas cosas

--- src/components/Pelicula/Pelicula.js
+++ src/components/Pelicula/Pelicula.js
@@ -4,11 +4,7 @@ import { Link } from "react-router-dom"
 class Pelicula extends Component {
     constructor(props) {
         super(props)
-        this.state = { verMas: false }
     }
-
-    verMas() { this.setState({ overview: this.props.overview }) }
-    verMenos() { this.setState({ overview: true }) }
 
     render() {
         return (
@@ -17,12 +13,6 @@ class Pelicula extends Component {
                 <img src={`https://image.tmdb.org/t/p/w200${this.props.poster_path}`} alt={this.props.title} />
                 </Link>
                 <h4>{this.props.title}</h4>
-                <p>{this.state.overview}</p>
-                {this.state.overview === this.props.overview ? (
-                    <button className="button" onClick={() => this.verMenos()}>Ver menos</button>
-                ) : (
-                    <button className="button" onClick={() => this.verMas()}>Ver más</button>
-                )}
             </div>
         )
     }
