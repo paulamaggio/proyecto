4877fb5c68e4f84994e7e69bcb24ee246a28a08d
JustinaVergara <vergaram@udesa.edu.ar>
2023-09-11 10:27:01 -0300

    ver mas y favoritos

--- src/components/Pelicula/Pelicula.js
+++ src/components/Pelicula/Pelicula.js
@@ -1,9 +1,19 @@
-import { Component } from "react"
+import { Component, Fragment } from "react"
 import { Link } from "react-router-dom"
 
 class Pelicula extends Component {
     constructor(props) {
         super(props)
+        this.state = { 
+            boton : false
+        }
+    }
+
+    verMenos(){
+        this.setState({boton:false})
+    }
+    verMas(){
+        this.setState({boton:true})
     }
 
     render() {
@@ -13,6 +23,15 @@ class Pelicula extends Component {
                 <img src={`https://image.tmdb.org/t/p/w200${this.props.poster_path}`} alt={this.props.title} />
                 </Link>
                 <h4>{this.props.title}</h4>
+                {this.state.boton ? 
+                <>
+                <p>{this.props.overview}</p>
+                <button onClick={() => this.verMenos()}>ver menos</button>
+                </>
+                :
+                <button onClick={() => this.verMas()}>ver mas</button>
+                } 
+                  
             </div>
         )
     }
--- src/screens/Detalle/index.js
+++ src/screens/Detalle/index.js
@@ -5,16 +5,58 @@ export default class index extends Component {
 
   constructor(props){
     super(props)
-    this.state = {pelicula : null}
+    this.state = 
+    {pelicula : null,
+    esFavorito : false}
   }
 
   componentDidMount(){
     fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}`, options)
     .then(resp => resp.json())
-    .then(data => this.setState({pelicula:data}))
+    .then(data => this.setState({pelicula:data}, () =>{
+      if (arrParseado !== null){
+        let valPelicula = arrParseado.includes(this.state.pelicula.id)
+        if(valPelicula){ 
+          this.setState({
+            esFavorito: true
+          })
+        }
+      }
+    }))
     .catch(err => console.log(err))
+    let storageFav = localStorage.getItem('favoritos')
+    let arrParseado = JSON.parse(storageFav)
+
+  }
+  
+  favear(idPelicula){
+    let storageFav = localStorage.getItem('favoritos')
+    if(storageFav === null){
+      let idFav = [idPelicula]
+      let idFavStrings = JSON.stringify(idFav)
+      localStorage.setItem('favoritos', idFavStrings)}
+    else{
+      let idPel = JSON.parse(storageFav)
+      idPel.push(idPelicula)
+      let idStrings = JSON.stringify(idPel)
+      localStorage.setItem('favoritos', idStrings)
+    }
+
+    this.setState({
+      esFavorito :true
+    })
   }
 
+  desFavear(idPelicula) {
+    let storageFav = localStorage.getItem('favoritos')
+    let arrParseado = JSON.parse(storageFav)
+    let favsFiltrados = arrParseado.filter((id)=> id !== idPelicula)
+    let arrString = JSON.stringify(favsFiltrados)
+    localStorage.setItem('favoritos', arrString)
+    this.setState({
+      esFavorito:false
+    })
+  }
   render(){
     console.log(this.props);
     return(
@@ -27,6 +69,13 @@ export default class index extends Component {
         <h5>Duracion: {this.state.pelicula.runtime}</h5>
         <h5>Sinópsis: {this.state.pelicula.overview}</h5>
         {/* <h5>Genero: {this.state.pelicula.}</h5> */}
+        {
+          this.state.esFavorito ?
+          <button onClick={()=> this.desFavear(this.state.pelicula.id)}>Sacar de favoritos</button> 
+          : 
+          <button onClick={() => this.favear(this.state.pelicula.id)}>Agregar a favoritos</button>
+        }
+       
       </div>
       :
       <h1 className=''>Cargando Peliculas...</h1>
